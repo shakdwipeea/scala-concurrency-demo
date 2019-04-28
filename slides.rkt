4c63009395947712ce1221902e8fadc41d36be0a
Akash Shakdwipeea <ashakdwipeea@gmail.com>
2019-04-29 02:50:36 +0530

    update slides

--- slides.rkt
+++ slides.rkt
@@ -1,15 +1,71 @@
 #lang slideshow
 
-(slide (t "using akka")
-       (item "If you want to create an example, you"
-             "can always do something with" (bt "Hello World!"))
-       (item "First step")
-       'next
-       (item "Second step"))
+(require net/sendurl
+         slideshow/code
+         slideshow/text)
 
-(slide (t "why akka")
- (item "Tentative third step")
- 'next
- (item "This isn't working... back up")
- 'next
- (item "Third step that works"))
+(define akka-bench-url "https://letitcrash.com/post/20397701710/50-million-messages-per-second-on-a-single")
+
+(slide (titlet "Actors in scala")
+       (item "objects which encapsulate state and behaviour")
+       'next
+       (item "communicate by exchaning message through mailbox")
+       'next
+       (item "split task up and delegate"))
+
+(slide (titlet "Distributed by design")
+       (item "local to remote from config")
+       'next
+       (item "routing and load balancing strategies present out of the box"))
+
+(slide (titlet "Actors")
+       (item "class with a queue")
+       'next
+       (item "handles one msg at a time")
+       'next
+       (item "extremely light weight")
+       'next
+       (clickback (item (tt "50 million msg / sec in 1 machine")) (λ () (send-url akka-bench-url))))
+
+(slide (titlet "Remote Actors")
+       'next
+       (scale (vl-append 4.0
+                         (item (bt "creating remote acros"))
+                         (codeblock-pict "akka {
+  actor {
+    deployment {
+      /sampleActor {
+        remote = \"akka.tcp://sampleActorSystem@127.0.0.1:2553\"
+      }
+    }
+  }
+}")
+                         (item (bt "looking up remote actors"))
+                         (codeblock-pict "akka.<protocol>://<actor system name>@<hostname>:<port>/<actor path>"))
+              0.75))
+
+(slide (titlet "Routers")
+       'next
+       (item "Specify load balancing for actor")
+       (codeblock-pict
+        "akka.actor.deployment {
+  /myRouter {
+    router = round-robin
+    nr-of-instances = 5
+  }
+}"))
+
+;; (item "ActorInitializedException => Stop")
+;;             (item "ActorKilledException => Stop")
+;;             (item "Exception => Restart")
+;;             (item "Throwable => Escalate")
+;;             (item "Otherwise => Escalate")
+
+(slide (titlet "Supervisors")
+       (item "Strategy Stop / Restart / Escalate ")
+       (item "Can be overwritten")
+       (item "default supervisor /users: GuardianActor"))
+
+(slide (titlet "Demo"))
+             
+
