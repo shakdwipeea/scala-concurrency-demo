fbb759257335649168b230fc3345630976c8cd1f
Akash Shakdwipeea <ashakdwipeea@gmail.com>
2019-04-28 16:43:52 +0530

    initial commit

---
+++ slides.rkt
@@ -0,0 +1,15 @@
+#lang slideshow
+
+(slide (t "using akka")
+       (item "If you want to create an example, you"
+             "can always do something with" (bt "Hello World!"))
+       (item "First step")
+       'next
+       (item "Second step"))
+
+(slide (t "why akka")
+ (item "Tentative third step")
+ 'next
+ (item "This isn't working... back up")
+ 'next
+ (item "Third step that works"))
